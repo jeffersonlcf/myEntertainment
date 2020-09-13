90f3e7d81ea555bfcadd125a3978c7d7d662609c
jeffersonlcf <jeffersonlcf@gmail.com>
2020-09-13 11:36:49 +0100

    Change to dark theme

--- resources/js/components/ratings/LikeComponent.vue
+++ resources/js/components/ratings/LikeComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-button-group>
+    <b-button-group :class="{ 'border-success': like === 1, 'border-danger': like === 0 }" class="border rounded">
         <b-button variant="border-0" @click="toggleLike(1)" :class="{ 'btn-success': like === 1 }" size="sm"><span class="icon">✔</span></b-button>
         <b-button variant="border-0" @click="toggleLike(0)" :class="{ 'btn-danger': like === 0 }" size="sm"><span class="icon">✖</span></b-button>
     </b-button-group>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,28 +8,28 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    <h2>Your recently rated films</h2>
+                    <h2>Your Recently Rated Films</h2>
                     <div class="row mb-3">
                     @foreach ($last_rated_films as $rating)
                         <div class="col-sm-3 d-flex flex-column justify-content-center w-50 mt-2">
-                            <a href="{{ route('me.show', ['me' => $rating->rateable->id]) }}">
-                                <figure class="figure">
-                                    <img src="{{ $rating->rateable->thumbnail }}" class="figure-img img-fluid rounded" alt="{{ $rating->rateable->title }}">
-                                    <figcaption class="figure-caption text-center">{{ $rating->rateable->title }}</figcaption>
+                            <a href="{{ route('me.show', ['me' => $rating->rateable->id]) }}" class="rounded shadow-dark mb-1" style="background-color: rgb(110, 103, 90);">
+                                <figure>
+                                    <img src="{{ $rating->rateable->thumbnail }}" class="figure-img img-fluid rounded-top" alt="{{ $rating->rateable->title }}">
+                                    <figcaption class="figure-caption text-center text-white">{{ $rating->rateable->title }}</figcaption>
                                 </figure>
                             </a>
                             <like-component class="mt-auto" url="{{ route('me.like', ['me' => $rating->rateable->id]) }}" :initial-like={{ json_encode($rating->like) }}></like-component>
                         </div>
                     @endforeach
                     </div>
-                    <h2>Your recently rated series</h2>
+                    <h2>Your Recently Rated Series</h2>
                     <div class="row mb-3">
                     @foreach ($last_rated_series as $rating)
                         <div class="col-sm-3 d-flex flex-column justify-content-center w-50 mt-2">
-                            <a href="{{ route('me.show', ['me' => $rating->rateable->id]) }}">
-                                <figure class="figure">
-                                    <img src="{{ $rating->rateable->thumbnail }}" class="figure-img img-fluid rounded" alt="{{ $rating->rateable->title }}">
-                                    <figcaption class="figure-caption text-center">{{ $rating->rateable->title }}</figcaption>
+                            <a href="{{ route('me.show', ['me' => $rating->rateable->id]) }}"  class="rounded shadow-dark mb-1" style="background-color: rgb(110, 103, 90);">
+                                <figure>
+                                    <img src="{{ $rating->rateable->thumbnail }}" class="figure-img img-fluid rounded-top" alt="{{ $rating->rateable->title }}">
+                                    <figcaption class="figure-caption text-center text-white">{{ $rating->rateable->title }}</figcaption>
                                 </figure>
                             </a>
                             <like-component class="mt-auto" url="{{ route('me.like', ['me' => $rating->rateable->id]) }}" :initial-like={{ json_encode($rating->like) }}></like-component>
